10db750744e146b4729323c6f522f3a8a6bc2ad3
Yunsu Choi <goodbye.tigris@gmail.com>
2012-04-23 03:15:29 +0900

    first commit

---
+++ gen/com/tigris/kakao/talk/theme/HoloLight/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tigris.kakao.talk.theme.HoloLight;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
